d5a18759644cfda566805d00b9d08594407e2f0c
Joom <jnwongsa@gmail.com>
2024-06-05 14:07:04 +0700

    update:ex form handling1/Joom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingInputText, setGreetingInputText] = useState("");
+  const [greetingText, setGreetingText] = useState("Greeting Message");
+
+  const handlerClick = () => {
+    setGreetingText(greetingInputText);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => {
+            setGreetingInputText(event.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handlerClick}>Update text</button>
       </div>
     </div>
   );
