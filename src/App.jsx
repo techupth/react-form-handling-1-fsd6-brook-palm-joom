fd29654f509d30a50f2757040b3a573924835c36
Krittayapornn <krittayaporn.ww@gmail.com>
2024-06-05 12:35:59 +0700

    feat: newGreetingMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("Update Text");
+  const [greetingText, setGreetingText] = useState("Greeting Message");
+
+  const handledClick = () => {
+    setGreetingText(inputText);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
+
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => {
+            setInputText(event.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handledClick}>Update text</button>
       </div>
     </div>
   );
